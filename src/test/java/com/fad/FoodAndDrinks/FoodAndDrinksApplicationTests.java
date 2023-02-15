1361c0b63a46c8f9a1a73191b59603a4bed027dc
Alex Ababii <alexandr.ababii@yopeso.com>
2023-02-15 16:32:50 +0100

    initial commit

---
+++ src/test/java/com/fad/FoodAndDrinks/FoodAndDrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fad.FoodAndDrinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAndDrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
